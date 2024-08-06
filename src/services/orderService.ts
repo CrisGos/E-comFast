e6500d7a739b3231798fdb486cbee6822303d533
CrisGos <cristianortega9509@gmail.com>
2024-08-05 21:33:13 -0500

    readme updated

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -1,6 +1,7 @@
 import OrderRepository from '../repositories/orderRepository';
 import { injectable, inject } from 'tsyringe';
 import { Order } from "../models/order";
+import ProductCartService from './productService';
 
 @injectable()
 export default class OrderService { // this class will receive params and / or body from controllers call and will insert it into repositories to call sequelize methods
@@ -14,7 +15,10 @@ export default class OrderService { // this class will receive params and / or b
         return await this.orderRepository.findById(id);
     }
 
-    async createOrders(order: Partial<Order>) { // This method will connect with repositorie of POST with order type
+    async createOrders() { // This method will connect with repositorie of POST with order type
+        const order: Partial<Order> = {
+            "total": ProductCartService.getProductsByCartId(id: number)
+        }
         return await this.orderRepository.create(order);
     }
 
@@ -26,3 +30,29 @@ export default class OrderService { // this class will receive params and / or b
         return await this.orderRepository.deleteById(id);
     }
 }
+
+
+// @injectable()
+// export default class OrderService { // this class will receive params and / or body from controllers call and will insert it into repositories to call sequelize methods
+//     constructor(@inject(OrderRepository) private orderRepository: OrderRepository) {}
+
+//     async getAllOrders() { // This method will connect with repositorie of GET
+//         return await this.orderRepository.findAll(); 
+//     }
+
+//     async getOrdersById(id: number) { // This method will connect with repositorie of GET by ID
+//         return await this.orderRepository.findById(id);
+//     }
+
+//     async createOrders(order: Partial<Order>) { // This method will connect with repositorie of POST with order type
+//         return await this.orderRepository.create(order);
+//     }
+
+//     async updateOrders(id: number, order: Partial<Order>) { // This method will connect with repositorie of UPDATE with order type
+//         return await this.orderRepository.update(id, order);
+//     }
+
+//     async deleteOrders(id: number) { // This method will connect with repositorie of DELETE
+//         return await this.orderRepository.deleteById(id);
+//     }
+// }
--- src/services/productCartService.ts
+++ src/services/productCartService.ts
@@ -14,6 +14,10 @@ export default class ProductCartService { // this class will receive params and 
         return await this.productCartRepository.findById(id);
     }
 
+    async getProductsByCartId(id: number) {
+        return await this.productCartRepository.findByCartId(id);
+    }
+
     async createProductCarts(productCart: Partial<ProductCart>) { // This method will connect with repositorie of POST with productCart type
         return await this.productCartRepository.create(productCart);
     }
@@ -26,3 +30,28 @@ export default class ProductCartService { // this class will receive params and 
         return await this.productCartRepository.deleteById(id);
     }
 }
+
+// @injectable()
+// export default class ProductCartService { // this class will receive params and / or body from controllers call and will insert it into repositories to call sequelize methods
+//     constructor(@inject(ProductCartRepository) private productCartRepository: ProductCartRepository) {}
+
+//     async getAllProductCarts() { // This method will connect with repositorie of GET
+//         return await this.productCartRepository.findAll(); 
+//     }
+
+//     async getProductCartsById(id: number) { // This method will connect with repositorie of GET by ID
+//         return await this.productCartRepository.findById(id);
+//     }
+
+//     async createProductCarts(productCart: Partial<ProductCart>) { // This method will connect with repositorie of POST with productCart type
+//         return await this.productCartRepository.create(productCart);
+//     }
+
+//     async updateProductCarts(id: number, productCart: Partial<ProductCart>) { // This method will connect with repositorie of UPDATE with productCart type
+//         return await this.productCartRepository.update(id, productCart);
+//     }
+
+//     async deleteProductCarts(id: number) { // This method will connect with repositorie of DELETE
+//         return await this.productCartRepository.deleteById(id);
+//     }
+// }
